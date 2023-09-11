d2702b9c312f8a9b73158dbe84386ff61574e635
BerdinaKatya <kuznkate42@gmail.com>
2023-09-11 16:20:11 +0500

    Initial Commit

---
+++ D&D handbook/Models/Race.swift
@@ -0,0 +1,16 @@
+//
+//  Race.swift
+//  D&D handbook
+//
+//  Created by Екатерина Теляубердина on 11.09.2023.
+//
+
+import Foundation
+
+struct Race: Decodable {
+    let index: String
+    let name: String
+    let url: String
+    let speed: Int
+    
+}
---
+++ D&D handbook/View Controllers/TableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewController.swift
+//  D&D handbook
+//
+//  Created by Екатерина Теляубердина on 11.09.2023.
+//
+
+import UIKit
+
+final class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
